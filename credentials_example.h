ca0898ddd822052e791c5bfec9a31ae2458c41b5
Charlie Steenhagen <csteen1005@gmail.com>
2020-02-21 13:20:39 -0500

    Initial Commit

---
+++ credentials_example.h
@@ -0,0 +1,11 @@
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+// This will be emailed to you upon project creation
+char auth[] = "BLYNK AUTH CODE";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "My WiFi Connection";
+char password[] = "examplePassword";
+#define API_KEY "ENTER_YOUR_API_KEY"  // your google apps API Token
+#define CHANNEL_ID "UCezJOfu7OtqGzd5xrP3q6WA" // makes up the url of channel
